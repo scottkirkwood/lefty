5262492d6ada8218e87dd525c881cc732741c00e
Scott Kirkwood <scott@forusers.com>
2013-06-15 13:48:29 -0400

    Version 1.1

--- make_zip.sh
+++ make_zip.sh
@@ -3,7 +3,7 @@
 # Copyright 2010 Google Inc. All Rights Reserved.
 # Author: scottkirkwood@google.com (Scott Kirkwood)
 
-ver=$(grep version manifest.json | cut -c15-17)
+ver=$(grep '"version"' manifest.json | cut -c15-17)
 echo "Version is: $ver"
 echo -n "Is this correct? (ctrl-c to abort)"
 read
